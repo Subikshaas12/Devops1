8b282fe6e6e4d55902cd997f05aff80f99d05a45
Subikshaas12 <subiksha.502261@sxcce.edu.in>
2025-01-30 18:16:33 +0530

    Add files via upload

---
+++ mavenproject3/src/main/java/Mavenproject3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+        
+
+
+    
+        System.out.println("Addition of two numbers");
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter a:");
+        int a=scanner.nextInt();
+        System.out.println("Enter b:");
+        int b=scanner.nextInt();
+        int c=a+b;
+        System.out.println("Sum:"+c);
+    }
+}
